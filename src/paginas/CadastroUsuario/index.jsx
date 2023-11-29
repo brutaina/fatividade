b3229cc73b9283ace14208b2bfa138498e9d4e50
brutaina <brunatainact@gmail.com>
2023-11-29 00:26:18 -0300

    CADASTRO

--- src/paginas/CadastroUsuario/index.jsx
+++ src/paginas/CadastroUsuario/index.jsx
@@ -1,57 +1,59 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import styles from "../../componentes/Conteudo/Conteudo.module.css";
 
 const CadastroUsuario = () => {
+    const [nome, setNome] = useState('');
+    const [email, setEmail] = useState('');
+    const [confirmacaoSenha, setConfirmacaoSenha] = useState('');
 
-    const [nome,setNome]=useState('')
-    const [email,setEmail]=useState('')
-    const [senha,setSenha]=useState('')
-    const [confirmacaoSenha,setconfirmacaoSenha]=useState('')
-
-    function handleNome(e){
-        setNome(e.target.value)
-    }
-    function handleEmail(e){
-        setEmail(e.target.value)
-    }
-    function handleSenha(e){
-        setSenha(e.target.value)
-    }
-    function handleConfirmacaoDeSenha(e){
-        setconfirmacaoSenha(e.target.value)
+    function handleNome(e) {
+        setNome(e.target.value);
     }
 
-    function handleSubmit(e){
-        if(senha==confirmacaoSenha){
-            const user={nome,email,senha}
-            localStorage.setItem('user',JSON.stringify(user))
-        }else{
-            alert("As senhas não coincidem")
+    function handleEmail(e) {
+        setEmail(e.target.value);
+    }
+
+    function handleConfirmacaoDeSenha(e) {
+        setConfirmacaoSenha(e.target.value);
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        // Validação de campos vazios
+        if (!nome || !email || !confirmacaoSenha) {
+            alert("Por favor, preencha todos os campos.");
+            return;
+        }
+
+        // Verificação se as senhas coincidem
+        if (confirmacaoSenha) {
+            const user = { nome, email, senha: confirmacaoSenha };
+            localStorage.setItem('user', JSON.stringify(user));
+            alert("Usuário salvo com sucesso!");
+        } else {
+            alert("A senha não pode estar vazia");
         }
     }
 
-  return (
+    return (
         <div id="div-cadastro-usuario" className={styles.conteudo}>
-          <h1>Cadastro de Usuário</h1>
-          <form>
-            <label htmlFor="cadastro_nome">Nome</label>
-            <input type="text" onChange={(e) => handleNome(e)} />
-            <br/>            <label htmlFor="cadastro_email">Email</label>
-            <input type="text" onChange={(e) => handleEmail(e)} />
-            <br/> 
-            <label htmlFor="cadastro_senha">Senha</label>
-            <input type="text" onChange={(e) => handleSenha(e)} />
-            <br/> 
-            <label htmlFor="cadastro_confirmacaoSenha">Confirmação de Senha</label>
-            <input type="text" onChange={(e) => handleConfirmacaoDeSenha(e)} />
-            <br/> 
-            <button onClick={(e)=>{
-                e.preventDefault();
-                handleSubmit(e)
-            }}>Salvar Usuario</button>
-          </form>
+            <h1>Cadastro de Usuário</h1>
+            <form className={styles.formulario}>
+                <label htmlFor="cadastro_nome">Nome</label>
+                <input type="text" onChange={(e) => handleNome(e)} />
+
+                <label htmlFor="cadastro_email">Email</label>
+                <input type="text" onChange={(e) => handleEmail(e)} />
+
+                <label htmlFor="cadastro_confirmacaoSenha">Confirmação de Senha</label>
+                <input type="password" onChange={(e) => handleConfirmacaoDeSenha(e)} />
+
+                <button onClick={(e) => handleSubmit(e)}>Salvar Usuário</button>
+            </form>
         </div>
-      );
+    );
 }
 
-export default CadastroUsuario
+export default CadastroUsuario;
