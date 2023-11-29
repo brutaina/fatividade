4cb156f6f98e36643e5b3e619853f04fa849e48e
brutaina <brunatainact@gmail.com>
2023-11-28 21:33:13 -0300

    inicio

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Rotas from './routes'
+
+/*
+Arquivo App JSX
+Componente funcional = Porque ele é uma função
+*/
+function App() {
+  return <Rotas/>
+}
+
+
+export default App;
